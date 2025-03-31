0e61c695af58a2398cf9f9da36f9e2d72ebe090a
GuilFranca <guilfranca27@gmail.com>
2025-03-31 18:47:29 -0300

    Aula 2 Definindo tipos - Finalizada

--- Aula_02/dist/js/typescript.js
+++ Aula_02/dist/js/typescript.js
@@ -8,8 +8,16 @@ qualquer = 22;
 const lista = [];
 // lista.push("Jhonathan", "Cachorros", 22, true, []);
 lista.push(13, 22.5, 22, 89, 1.58);
+// Enum
+var TipoTransacao;
+(function (TipoTransacao) {
+    TipoTransacao["DEPOSITO"] = "Deposito";
+    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
+    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
+})(TipoTransacao || (TipoTransacao = {}));
 const novaTransacao = {
-    tipoTransacao: '',
+    // tipoTransacao : '',
+    tipoTransacao: TipoTransacao.DEPOSITO, // Agora tem que seguir o padrão da Enum para ser aprovado
     date: new Date(),
     valor: 0
 };
--- Aula_02/src/typescript.ts
+++ Aula_02/src/typescript.ts
@@ -14,13 +14,22 @@ lista.push(13, 22.5, 22, 89, 1.58);
  
 // Tipos Personalizados (Type Alias)
 type Transacao = {
-    tipoTransacao: string;
+    // tipoTransacao: string; 
+    tipoTransacao: TipoTransacao; // será do tipo Enum TipoTransacao
     date: Date;
     valor: number;
 }
 
+// Enum
+enum TipoTransacao {
+    DEPOSITO = 'Deposito',
+    TRANSFERENCIA = 'Transferência',
+    PAGAMENTO_BOLETO = 'Pagamento de Boleto'
+}
+
 const novaTransacao: Transacao = {
-    tipoTransacao : '',
+    // tipoTransacao : '',
+    tipoTransacao : TipoTransacao.DEPOSITO, // Agora tem que seguir o padrão da Enum para ser aprovado
     date: new Date(),
     valor: 0
 }
